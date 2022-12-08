07f6a6b7c754bd08a5d36ac09d42f9b8a03d9dd4
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-08 23:23:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,28 @@
+#include "lib.h"
+bool conv(char &z) {
+    int c = 0, d = 0;
+    for (int i = 65; i < 91; i++) {
+        char s = i;
+        if (z == s) {
+            z = s + 32;
+            c = 1;
+            d = 1;
+            return true;
+            break;
+        }
+    }
+    if (d == 0) {
+        for (int i = 97; i < 123; i++) {
+            char s = i;
+            if (z == s) {
+                z = s - 32;
+                c = 1;
+                return true;
+                break;
+            }
+        }
+    }
+    if (c == 0) {
+        return false;
+    }
 
