1768e5e5bad53c7854c2f884a80cf280278b22a9
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-08 23:26:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "lib.h"
+
 using namespace std;
 
 int main() {
     char z;
-    cin>>z;
-   if(conv(z)){
-       cout<<z;
-   }else{
-       cout<<"errore";
-   }
+    cin >> z;
+    if(con(z)){
+        cout << z << endl;
+    }else{
+        cout << "errore" << endl;
+    }
     return 0;
 }
